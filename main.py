de4ab9a885a350f5fc341a2da1c44cc13c5c49ee
Антон Кулик <anton.smile.1998@gmail.com>
2024-02-09 19:08:36 +0300

    Изменены названия переменных

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import threading
-from app.delete_old_folders_script import check_and_remove_old_files
-from config.settings import FOLDER_PATH
+from app.delete_old_folders_script import check_old_files
+from config.settings import FOLDERS_PATH
 from config.settings import DAYS_TO_KEEP
 from config.settings import TIME_TO_SLEEP
 
 def main(directory_path, days_to_keep, time_to_sleep):
     # Создаем поток для выполнения функции в отдельном потоке
-    thread = threading.Thread(target=check_and_remove_old_files, args=(directory_path, days_to_keep, time_to_sleep))
-    thread.daemon = True  # Поток завершится, если основной поток завершится
+    thread = threading.Thread(target=check_old_files, args=(directory_path, days_to_keep, time_to_sleep))
+    # thread.daemon = True  # Поток завершится, если основной поток завершится
     thread.start()
 
 if __name__ == "__main__":
-    main(FOLDER_PATH, DAYS_TO_KEEP, TIME_TO_SLEEP)
+    main(FOLDERS_PATH, DAYS_TO_KEEP, TIME_TO_SLEEP)
